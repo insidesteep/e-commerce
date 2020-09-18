ed34d4a962910495e83e101ae188231006741689
insidesteep <salaxiddinovsuxrob@gmail.com>
2020-09-18 20:51:10 +0300

    Add slider & style

---
+++ js/slick.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev" ><img src="images/icons/left-arrow.svg"></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/icons/right-arrow.svg"></button>'
+    })
+})
